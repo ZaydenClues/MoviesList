44f36e65007a9b74eaf6ae497a0861e3c5ad7519
ZaydenClues <zaydenclues.unt@gmail.com>
2024-03-03 17:27:33 +0530

    Initial Commit

---
+++ movies/src/test/java/dev/saran/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.saran.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
